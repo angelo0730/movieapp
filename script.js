dd96191718fac1383cf3e77ca2a01d238bd6ce33
angelo0730 <137179844+angelo0730@users.noreply.github.com>
2024-01-10 15:14:42 +0800

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //TMDB 
 
-const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+const API_KEY = 'api_key=f963466200dc6de26d67255f760324c0';
 const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
